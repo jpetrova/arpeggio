6e9bf6347768f01957f615b6474ffd8e0632688d
Julia Petrova <jul.petrova91@gmail.com>
2019-06-01 00:56:24 +0300

    update sandwiches

--- sandwiches.js
+++ sandwiches.js
@@ -1,11 +1,11 @@
 // Бутерброды с сыром, колбасой и вареньем в сотношении 5:7:8. Всего бутербродов 120. Сколько из них с колбасой?
 
-function parts(cheese, ham, jam) {
+function parts(cheese, ham, jam, sandwiches) {
   var n = cheese + ham + jam
-  var a = 120 / n
+  var a = sandwiches / n
   var b = ham * a
   return b
 }
 
-var x = parts(5, 7, 8)
+var x = parts(5, 7, 8, 120)
 console.log(x == 42)
