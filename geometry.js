598f5f7ba2eeeed07239437a632106e0a2bedb4f
Julia Petrova <jul.petrova91@gmail.com>
2019-06-01 01:57:01 +0300

    add large angle

--- geometry.js
+++ geometry.js
@@ -7,3 +7,14 @@ function triangleArea(a, h) {
 
 var x = triangleArea(3, 4)
 console.log(x == 6)
+
+// В треугольнике сумма двух равных углов больше третьего на 6 градусов. Найдите больший угол треугольника
+
+function largeAngle(n) {
+  var sum = 180
+  var b = sum - n
+  var x = b / 2
+  return x
+}
+
+console.log('largeAngle(6) == 87 -> ' + (largeAngle(6) == 87))
