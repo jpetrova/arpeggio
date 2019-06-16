4ef50986f7e6144939fd3445a1a6ee26441ac80a
Julia Petrova <jul.petrova91@gmail.com>
2019-06-17 01:35:35 +0300

    Rename and fix list

--- list.js
+++ list.js
@@ -1,12 +1,11 @@
 /**
  * Модуль для работы с массивом как со списком.
- * 1. пустой ли массив
- * 2. откусить голову
- * 3. сохранить хвост и передать его дальше
- * 4. присоединить голову к другому массиву
  */
 
 const List = {
+ /* 
+  * 1. пустой ли массив
+  */
     isEmpty: function isEmpty(xs) {
       if (xs.length == 0) {
         return true
@@ -14,22 +13,47 @@ const List = {
         return false
       }
     },
+ /* 
+  * 2. откусить голову
+  */
     head: function head(xs) {
       const h = xs[0]
       return h
     },
+ /*
+  * 3. сохранить хвост и передать его дальше
+  */
     tail: function tail(xs) {
       const ys = []
       const ys2 = ys.concat(xs)
       const h = ys2.splice(0, 1)
       return ys2
     },
+ /*
+  * 4. присоединить голову к другому массиву
+  */
     prepend: function prepend(xs, s) {
       const s1 = [s]
       const ys = s1.concat(xs)
       return ys
+    },
+ /**
+  * 5. развернуть мыссив
+  */
+     reverse: function reverse(xs) {
+     function go(xs, ys) {
+       if (List.isEmpty(xs) == true) {
+        return ys
+       }
+       else {
+       const h = List.head(xs)
+       const ys1 = List.prepend(ys, h)
+       const xs1 = List.tail(xs)
+       return go(xs1, ys1)
+       }
+     } 
+       return go(xs, [])
     }
-    
   }
   
   
@@ -38,31 +62,12 @@ const List = {
   const test3 = List.head([1,2,3])
   const test4 = List.tail([1,2,3])
   const test5 = List.prepend([1,2,3], 0)
+  const test6 = List.reverse([1,2,3])
+  
   console.log('List.isEmpty([1,2,3]) = ', test1)
   console.log('List.isEmpty([]) = ', test2)
   console.log('List.head([1,2,3]) = ', test3)
   console.log('List.tail([1,2,3]) = ', test4)
   console.log('List.prepend([1,2,3], 0) = ', test5)
-  
-  /**
-   * На вход подается массив, необходимо развернуть его
-   */
-  
-  function reverse(xs) {
-   function go(xs, ys) {
-     if (List.isEmpty(xs) == true) {
-      return ys
-     }
-     else {
-     const h = List.head(xs)
-     const ys1 = List.prepend(ys, h)
-     const xs1 = List.tail(xs)
-     return go(xs1, ys1)
-     }
-   } 
-     return go(xs, [])
-  }
-  
-  const rev = reverse([1,2,3])
-  console.log('reverse [1,2,3] = ', rev)
+  console.log('List.reverse [1,2,3] = ', test6)
   
