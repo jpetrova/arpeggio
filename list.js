b72ab70cc9069f4860fd58c4f74159c8fde03b2f
Julia Petrova <jul.petrova91@gmail.com>
2019-06-17 02:24:11 +0300

    Fix list

--- list.js
+++ list.js
@@ -1,71 +1,76 @@
-  /**
-   * Модуль для работы с массивом как со списком.
-   */
- const List = {
+/**
+* Модуль для работы с массивом как со списком.
+*/
+const List = {
+
   /** 
-   * Пустой ли список
-   */
-   isEmpty: function isEmpty(xs) {
-     if (xs.length == 0) {
-       return true
-     } else {
-        return false
-     }
-   },
-  /** 
-   * Голова списка
-   */
-   head: function head(xs) {
-     const h = xs[0]
-     return h
-   },
-  /**
-   * Хвост списка
-   */
-   tail: function tail(xs) {
-     const ys = []
-     const ys2 = ys.concat(xs)
-     const h = ys2.splice(0, 1)
-     return ys2
-   },
-  /**
-   * Вставка в начало списка
-   */
-   prepend: function prepend(xs, s) {
-     const s1 = [s]
-     const ys = s1.concat(xs)
-     return ys
-   },
-   /**
-    * Развернуть список
-    */
-   reverse: function reverse(xs) {
-     function go(xs, ys) {
-       if (List.isEmpty(xs)) {
-         return ys
-       } else {
-         const h = List.head(xs)
-         const ys1 = List.prepend(ys, h)
-         const xs1 = List.tail(xs)
-         return go(xs1, ys1)
-       }
-     } 
-       return go(xs, [])
+  * Является ли список пустым
+  */
+  isEmpty: function isEmpty(xs) {
+    if (xs.length == 0) {
+      return true
+    } else {
+      return false
     }
+  },
+
+  /** 
+  * Голова списка
+  */
+  head: function head(xs) {
+    const h = xs[0]
+    return h
+  },
+
+  /**
+  * Хвост списка
+  */
+  tail: function tail(xs) {
+    const ys = []
+    const ys2 = ys.concat(xs)
+    const h = ys2.splice(0, 1)
+    return ys2
+  },
+
+  /**
+  * Вставка в начало списка
+  */
+  prepend: function prepend(xs, s) {
+    const s1 = [s]
+    const ys = s1.concat(xs)
+    return ys
+  },
+
+  /**
+  * Развернуть список
+  */
+  reverse: function reverse(xs) {
+    function go(xs, ys) {
+      if (List.isEmpty(xs)) {
+        return ys
+      } else {
+        const h = List.head(xs)
+        const ys1 = List.prepend(ys, h)
+        const xs1 = List.tail(xs)
+        return go(xs1, ys1)
+      }
+    } 
+    return go(xs, [])
   }
-  
-  
-  const test1 = List.isEmpty([1,2,3])
-  const test2 = List.isEmpty([])
-  const test3 = List.head([1,2,3])
-  const test4 = List.tail([1,2,3])
-  const test5 = List.prepend([1,2,3], 0)
-  const test6 = List.reverse([1,2,3])
-  
-  console.log('List.isEmpty([1,2,3]) = ', test1)
-  console.log('List.isEmpty([]) = ', test2)
-  console.log('List.head([1,2,3]) = ', test3)
-  console.log('List.tail([1,2,3]) = ', test4)
-  console.log('List.prepend([1,2,3], 0) = ', test5)
-  console.log('List.reverse [1,2,3] = ', test6)
-  
+
+}
+
+
+const test1 = List.isEmpty([1,2,3])
+const test2 = List.isEmpty([])
+const test3 = List.head([1,2,3])
+const test4 = List.tail([1,2,3])
+const test5 = List.prepend([1,2,3], 0)
+const test6 = List.reverse([1,2,3])
+
+console.log('List.isEmpty([1,2,3]) = ', test1)
+console.log('List.isEmpty([]) = ', test2)
+console.log('List.head([1,2,3]) = ', test3)
+console.log('List.tail([1,2,3]) = ', test4)
+console.log('List.prepend([1,2,3], 0) = ', test5)
+console.log('List.reverse [1,2,3] = ', test6)
