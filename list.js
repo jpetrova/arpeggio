c7d4f0613be2a6716510cfc7a9a65478b4bb74b0
Julia Petrova <jul.petrova91@gmail.com>
2019-06-23 01:22:08 +0300

    Fix ListOps

--- list.js
+++ list.js
@@ -50,20 +50,6 @@ const ListOps = {
     }
   },
 
-  /** 
-   * Голова списка
-   */
-  head: function head(xs) {
-    return xs.head
-  },
-
-  /**
-   * Хвост списка
-   */
-  tail: function tail(xs) {
-    return xs.tail
-  },
-
   /**
    * Вставка в начало списка
    */
@@ -76,12 +62,12 @@ const ListOps = {
    */
   reverse: function reverse(xs) {
     function go(xs, ys) {
-      if (ListOps.isEmpty(xs) == true) {
+      if (ListOps.isEmpty(xs)) {
         return ys
       } else {
-        const x = ListOps.head(xs)
+        const x = xs.head
         const ys1 = ListOps.prepend(x, ys)
-        const xs1 = ListOps.tail(xs)
+        const xs1 = xs.tail
         return go(xs1, ys1)
       }
     }
@@ -90,13 +76,17 @@ const ListOps = {
 
 }
 
+
+const oneElementList = List.Cons(1, List.Nil)
+const emptyList = List.Nil
+
 console.log('List.Nil = ', List.Nil)
 console.log('List.Cons(1, List.Nil) = ', List.Cons(1, List.Nil))
 console.log('ListOps.isEmpty(List.Nil) = ', ListOps.isEmpty(List.Nil))
 console.log('ListOps.isEmpty(List.Cons(1, List.Nil)) = ', ListOps.isEmpty(List.Cons(1, List.Nil)))
-console.log('ListOps.head(List.Nil) = ', ListOps.head(List.Nil))
-console.log('ListOps.head(List.Cons(1, List.Nil)) = ', ListOps.head(List.Cons(1, List.Nil)))
-console.log('ListOps.tail(List.Nil) = ', ListOps.tail(List.Nil))
-console.log('ListOps.tail(List.Cons(1, List.Nil)) = ', ListOps.tail(List.Cons(1, List.Nil)))
+console.log('oneElementList.head = ', oneElementList.head)
+console.log('emptyList.head = ', emptyList.head)
+console.log('oneElementList.tail = ', oneElementList.tail)
+console.log('emptyList.tail = ', emptyList.tail)
 console.log('ListOps.prepend(2, List.Cons(1, List.Nil) = ', ListOps.prepend(2, List.Cons(1, List.Nil)))
 console.log('ListOps.reverse(List.Cons(1, List.Cons(2, List.Nil)) = ', ListOps.reverse(List.Cons(1, List.Cons(2, List.Nil))))
