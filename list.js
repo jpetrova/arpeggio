3de905cab6c56b7a729bba6372ed408e5d7f28c2
Julia Petrova <jul.petrova91@gmail.com>
2019-06-26 02:06:21 +0300

    Add map to ListOps

--- list.js
+++ list.js
@@ -95,76 +95,45 @@ const ListOps = {
    */
   toString: function toString(xs) {
     return 'List(' + ListOps.mkString(xs, ', ') + ')'
-  }
+  },
+
+  /**
+   * Функция map
+   */
+  map: function map(xs, f) {
+    function go(xs, ys) {
+      if (ListOps.isEmpty(xs)) {
+        return ListOps.reverse(ys)
+      } else {
+        const x = f(xs.head)
+        const ys1 = ListOps.prepend(x, ys)
+        const xs1 = xs.tail
+        return go(xs1, ys1)
+      }
+    }
+    return go(xs, List.Nil)
+  }  
 
 }
 
 /**
- * Функция, которая принимает список чисел, возвращает новый список, каждый эллемент которого умножен на 2
- * [1, 2, 3, 4] -> [2, 4, 6, 8]
+ * Функция, которая принимает список и возвращает строку
  */
-function doubleList(xs) {
-  function go(xs, ys) {
-    if (ListOps.isEmpty(xs)) {
-      return ListOps.reverse(ys)
-    } else {
-      const x = xs.head * 2
-      const ys1 = ListOps.prepend(x, ys)
-      const xs1 = xs.tail
-      return go(xs1, ys1)
-    }
-  }
-  return go(xs, List.Nil)
+function f1(x) {
+  return x.toString()
 }
 
 /**
  * Функция, которая принимает список строк и возвращает список, где все строки прописными буквами
  * 'a, b, c, d', 'cbhi'  -> 'A, B, C, D, CBHI'
- * 
  */
-function capitalLetters(xs){
-  function go(xs, ys) {
-    if (ListOps.isEmpty(xs)) {
-      return ListOps.reverse(ys)
-    } else {
-      const x = xs.head
-      const x0 = x.toUpperCase()
-      const ys1 = ListOps.prepend(x0, ys)
-      const xs1 = xs.tail
-      return go(xs1, ys1)
-    }
-  }
-  return go(xs, List.Nil)
-}
-
-/**
- * Написать функцию, которая принимает список целых чисел и возвращает список, в котором каждый эл-т
- * возведен в степень значения этого эл-та
- * [2, 3, 4] -> [4, 27, 256]
- */
-function exponent(xs) {
-  function go(xs, ys) {
-    if (ListOps.isEmpty(xs)) {
-      return ListOps.reverse(ys)
-    } else {
-      const s = xs.head
-      const x = Math.pow(s, s)
-      const ys1 = ListOps.prepend(x, ys)
-      const xs1 = xs.tail
-      return go(xs1, ys1)
-    }
-  }
-  return go(xs, List.Nil)
-}
-
-function f1(x) {
-  return x.toString()
-}
-
 function f2(x) {
   return x.toUpperCase()
 }
 
+/**
+ * Функция, которая принимает число и возвращает симфол с таким кодом из таблицы ASCII
+ */
 function f3(x) {
   return String.fromCharCode(x)
 }
@@ -173,25 +142,29 @@ function f4(x) {
   return x + 96
 }
 
-function map(xs, f) {
-  function go(xs, ys) {
-    if (ListOps.isEmpty(xs)) {
-      return ListOps.reverse(ys)
-    } else {
-      const x = f(xs.head)
-      const ys1 = ListOps.prepend(x, ys)
-      const xs1 = xs.tail
-      return go(xs1, ys1)
-    }
-  }
-  return go(xs, List.Nil)
+/**
+ * Функция, которая принимает список чисел, возвращает новый список, каждый эллемент которого умножен на 2
+ * [1, 2, 3, 4] -> [2, 4, 6, 8]
+ */
+function f5(x) {
+  return x * 2
 }
 
+/**
+ * Написать функцию, которая принимает список целых чисел и возвращает список, в котором каждый эл-т
+ * возведен в степень значения этого эл-та
+ * [2, 3, 4] -> [4, 27, 256]
+ */
+function f6(x) {
+  return Math.pow(x, x)
+}
+
+
 const data = List.Cons(1, List.Cons(2, List.Cons(3, List.Nil)))
-const test1 = map(data, f4)
-const test2 = map(test1, f1)
-const test3 = map(test2, f3)
-const test4 = map(test3, f2)
+const test1 = ListOps.map(data, f4)
+const test2 = ListOps.map(test1, f1)
+const test3 = ListOps.map(test2, f3)
+const test4 = ListOps.map(test3, f2)
 
 console.log('data = ', ListOps.toString(data))
 console.log('map(data, f1) =', ListOps.toString(test1))
