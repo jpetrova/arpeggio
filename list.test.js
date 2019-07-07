7be431419ac98efecc34fd69ea902578400191d4
Julia Petrova <jul.petrova91@gmail.com>
2019-07-08 00:45:55 +0300

    Add ListOps tests

--- list.test.js
+++ list.test.js
@@ -1,4 +1,5 @@
 const List = require('./list').List
+const ListOps = require('./list').ListOps
 
 const Cons = List.Cons
 const Nil = List.Nil
@@ -42,5 +43,88 @@ describe('fromString', () => {
     const xs = List.fromString('12')
     const expected = Cons('1', Cons('2', Nil))
     expect(xs).toStrictEqual(expected)
+  })
+})
+
+describe('isEmpty', () => {
+  
+  test('empty', () => {
+    const xs = Nil
+    const expected = true
+    expect(ListOps.isEmpty(xs)).toStrictEqual(expected)
+  })
+  
+  test('not empty', () => {
+    const xs = Cons(1, Cons(2, Nil))
+    const expected = false
+    expect(ListOps.isEmpty(xs)).toStrictEqual(expected)
+  })
+})
+
+test('prepend', () => {
+  const xs = Cons(1, Cons(2, Nil))
+  const expected = Cons(0, Cons(1, Cons(2, Nil)))
+  expect(ListOps.prepend(0, xs)).toStrictEqual(expected)
+})
+
+describe('reverse', () => {
+  
+  test('Nil', () => {
+    const xs = Nil
+    const expected = Nil
+    expect(ListOps.reverse(xs)).toStrictEqual(expected)  
+  })
+  
+  test('one element', () => {
+    const xs = Cons(1, Nil)
+    const expected = Cons(1, Nil)
+    expect(ListOps.reverse(xs)).toStrictEqual(expected)
+  })
+  
+  test('many elements', () => {
+    const xs = Cons(1, Cons(2, Nil))
+    const expected = Cons(2, Cons(1, Nil))
+    expect(ListOps.reverse(xs)).toStrictEqual(expected)
+  })
+})
+
+describe('mkString', () => {
+
+  test('Nil', () => {
+    const xs = Nil
+    const expected = ''
+    expect(ListOps.mkString(xs, ', ')).toStrictEqual(expected)  
+  })
+
+  test('one element', () => {
+    const xs = Cons(1, Nil)
+    const expected = '1'
+    expect(ListOps.mkString(xs, ', ')).toStrictEqual(expected)  
+  })
+
+  test('many elements', () => {
+    const xs = Cons(1, Cons(2, Nil))
+    const expected = '1, 2'
+    expect(ListOps.mkString(xs, ', ')).toStrictEqual(expected)  
+  })
+})
+
+describe('toString', () => {
+  test('Nil', () => {
+    const xs = Nil
+    const expected = 'List()'
+    expect(ListOps.toString(xs)).toStrictEqual(expected)  
+  })
+
+  test('one element', () => {
+    const xs = Cons(1, Nil)
+    const expected = 'List(1)'
+    expect(ListOps.toString(xs)).toStrictEqual(expected)  
+  })
+
+  test('many elements', () => {
+    const xs = Cons(1, Cons(2, Nil))
+    const expected = 'List(1, 2)'
+    expect(ListOps.toString(xs)).toStrictEqual(expected)  
   })
 })
