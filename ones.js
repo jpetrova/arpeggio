1d4c195299c2e69b8264b18d2bf058eaae277bbd
Julia Petrova <jul.petrova91@gmail.com>
2019-07-13 18:25:34 +0300

    Add ones tests

--- ones.js
+++ ones.js
@@ -1,11 +1,8 @@
 const List = require('./list').List
 const ListOps = require('./list').ListOps
 
-const Cons = List.Cons
-const Nil = List.Nil
-
 /**
- * На вход подается последовательность чисел. Необходимо преобразовать ее в двоичный код
+ * На вход подается число. Необходимо преобразовать ее в двоичный код
  * и посчитать количество единиц. Последовательность всегда целочисленная.
  * Пример:
  * 1234 -> '10011010010' количество единиц = 5
@@ -17,3 +14,5 @@ function ones(n) {
   const xs = ListOps.map(cs, x => parseInt(x, 10))
   return ListOps.foldLeft(xs, 0, (acc, x) => acc + x)
 }
+
+module.exports = ones
